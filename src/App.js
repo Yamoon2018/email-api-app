f030826e26855fc1a8b8df2a03831e850c9684ae
Yamoon2018 <yamin.barakat@gmail.com>
2020-11-19 22:48:01 +0400

    few changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
+  const [sender_name, setSenderName] = useState("");
+  const [sender_email, setSenderEmail] = useState("");
+  const [receiver_name, setReceiverName] = useState("");
+  const [receiver_email, setReceiverEmail] = useState("");
+  const [email_subject, setEmailSubject] = useState("");
+  const [email_body, setEmailBody] = useState("");
+  const [email_status, setEmailStatus] = useState("");
+
+  const handleSubmit=(e)=>{
+     e.target.value
+
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <form onSubmit={handleSubmit} method="post">
+          Sender's Name<input placeholder="Sender's name..." type="text" name="sender_name" /> 
+          <p></p>
+          Sender's Email<input placeholder="Sender's email..." type="text" name="sender_email"  />
+          <p></p>
+          Receiever's Name<input placeholder="Receiver's name..." type="text" name="receiver_name" /> 
+          <p></p>
+          Receiever's Email(s)<input placeholder="Receiver's Email.." type="text" name="receiver_email" />
+          <p></p>
+          Subject <input placeholder="Subject..." type="text" name="email_subject" /> 
+          <p></p>
+          Message          
+          <textarea name="email_body" />
+          <p></p>
+          Email Status
+          <select onChange={handleChange} name="email_status" value={email_status_form} >          
+          <option value="all">Send one email to all receievers</option>
+          <option value="each">Send one email to each receiever</option>          
+        </select>
+          <p></p>
+          
+          <button name="btn_sendEmail" value ="btn_sendEmail">Send Email</button>
+          
+          </form>
       </header>
     </div>
   );
